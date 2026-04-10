4b4ad0901010092a0d7ab36917180289658e275b
Noah Gullo <noahgulloingram@gmail.com>
2026-04-09 20:13:00 -0700

    Add task class and minor styling changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./styles.css"
+
+class Task{
+    constructor(title, description, dueDate, priority, notes){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.complete = false;
+        this.notes = notes;
+    }
+
+    getCompletionStatus(){
+        return this.complete;
+    }
+
+    toggleComplete(){
+        this.complete = !this.complete;
+    }
+}
