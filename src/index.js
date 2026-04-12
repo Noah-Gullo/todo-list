68827cb6e42496ef0f9f2dfd0df96b8f04ca237e
Noah Gullo <noahgulloingram@gmail.com>
2026-04-11 21:04:16 -0700

    Add private instance var and require new keyword for classes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import "./styles.css"
 
 class Task{
+    #title;
+    #description;
+    #duedate;
+    #priority;
+    #expanded = false;
+    #complete = false;
+    #notes;
+
     constructor(title, description, dueDate, priority, notes){
+        if(!new.target){
+            throw new TypeError("Called Task constructor without new");
+        }
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
-        this.complete = false;
         this.notes = notes;
     }
 
@@ -17,10 +27,21 @@ class Task{
     toggleComplete(){
         this.complete = !this.complete;
     }
+
+    toggleExpand(){
+        this.expanded = !this.expanded;
+    }
 }
 
 class Project{
+    #name;
+    #icon;
+    #todolist
+    
     constructor(name, icon, todoList){
+        if(!new.target){
+            throw new TypeError("Called Project constructor without new");
+        }
         this.name = name;
         if(icon){
             this.icon = icon;
@@ -28,3 +49,6 @@ class Project{
         this.todoList = todoList;
     }
 }
+
+const a = new Task("a", "b", "c", "d", "e");
+a.title;
