b70a1f0bf5ac0cf0884ae8cda93b11f3b0b57d0b
Noah Gullo <noahgulloingram@gmail.com>
2026-04-15 20:14:33 -0700

    Fix project structure so every task/project is under a div

--- src/scripts/render.js
+++ src/scripts/render.js
@@ -3,19 +3,25 @@ import { projects, tasks } from "./application.js"
 export function renderTasks() {
     const taskContainer = document.getElementById("taskContainer");
     for(let i = 0; i < tasks.length; i++){
+        const taskDiv = document.createElement("div");
+        taskDiv.setAttribute("class", "task");
         const title = document.createElement("p");
         title.textContent = tasks[i].getTitle();
         
-        taskContainer.appendChild(title);
+        taskDiv.appendChild(title);
+        taskContainer.appendChild(taskDiv);
     }
 }
 
 export function renderProjects(){
     const projectContainer = document.getElementById("projectContainer");
     for(let i = 0; i < projects.length; i++){
+        const projectDiv = document.createElement("div");
+        projectDiv.setAttribute("class", "project");
         const name = document.createElement("p");
         name.textContent = projects[i].getName();
         
-        projectContainer.appendChild(name);
+        projectDiv.appendChild(name);
+        projectContainer.appendChild(projectDiv);
     }
 }
